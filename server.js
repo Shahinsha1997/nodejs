38d793397a7a0a55bd748874ae3df4119b2e36cf
Shahinsha1997 <35768512+Shahinsha1997@users.noreply.github.com>
2024-07-14 10:05:08 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const http= require('http');
+
+const port = 3000;
+const host = '0.0.0.0';
+const server =  http.createServer((req,res)=>{
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Zeet Node');
+})
+
+server.listen(port, host, ()=>{
+  console.log(`Server running at http://${host}:${port}/`);
+});
